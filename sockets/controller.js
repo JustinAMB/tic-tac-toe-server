1b1be08b6b7078d9a22fdd040f183f3e22f99cb0
JustinAMB <Justinmb@outlook.es>
2022-02-25 14:35:54 -0600

    se cambia el nombre del emit de iniciarPartida

--- sockets/controller.js
+++ sockets/controller.js
@@ -4,12 +4,12 @@ const jugadores = new Jugadores();
 const socketController = (cliente) => {
 
     console.log('Jugador conectado', cliente.id);
-    cliente.on('iniciarPartida', (data, callback) => {
+    cliente.on('iniciarPartida', (data, callback = Function) => {
         cliente.join(data.sala);
 
-        jugadores.agregarJugador(cliente.id, data.nombre, data.sala);
+        jugadores.agregarJugador(cliente.id, data.nombre, data.sala, data.created);
         console.log(jugadores.getJugadoresPorSala(data.sala));
-        cliente.broadcast.to(data.sala).emit('listaJugador', jugadores.getJugadoresPorSala(data.sala));
+        cliente.broadcast.to(data.sala).emit('iniciarPartida', jugadores.getJugadoresPorSala(data.sala));
 
 
         callback(jugadores.getJugadoresPorSala(data.sala));
