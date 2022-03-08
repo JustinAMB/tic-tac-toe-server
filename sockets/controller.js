3e9e32f623bcbc7006239f94ad6f46f72c648eca
JustinAMB <Justinmb@outlook.es>
2022-03-07 19:34:26 -0600

    se crea un socket para reiniciar partida

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,52 +1,57 @@
-const { Jugadores } = require('../models/jugadores');
+ const { Jugadores } = require('../models/jugadores');
 
-const jugadores = new Jugadores();
-const socketController = (cliente) => {
+ const jugadores = new Jugadores();
+ const socketController = (cliente) => {
 
-    console.log('Jugador conectado', cliente.id);
-    cliente.on('iniciarPartida', (data, callback = Function) => {
-        try {
-            cliente.join(data.sala);
+     console.log('Jugador conectado', cliente.id);
+     cliente.on('iniciarPartida', (data, callback = Function) => {
+         try {
+             cliente.join(data.sala);
 
-            jugadores.agregarJugador(cliente.id, data.nombre, data.sala, data.created);
-            console.log(jugadores.getJugadoresPorSala(data.sala));
-            cliente.broadcast.to(data.sala).emit('iniciarPartida', jugadores.getJugadoresPorSala(data.sala));
-            callback(jugadores.getJugadoresPorSala(data.sala));
-        } catch (err) {
+             jugadores.agregarJugador(cliente.id, data.nombre, data.sala, data.created);
+             console.log(jugadores.getJugadoresPorSala(data.sala));
+             cliente.broadcast.to(data.sala).emit('iniciarPartida', jugadores.getJugadoresPorSala(data.sala));
+             callback(jugadores.getJugadoresPorSala(data.sala));
+         } catch (err) {
 
-            callback();
-        }
+             callback();
+         }
 
-    });
-    cliente.on('jugada', (data, callback = Function) => {
-        console.log(data);
-        cliente.broadcast.to(data.sala).emit('jugada', data);
-        //callback(data);
-    });
-    cliente.on('ganador', (data, callback = Function) => {
-        console.log(data);
-        cliente.broadcast.to(data.sala).emit('ganador', data);
-        //callback(data);
-    });
-    cliente.on('disconnect', () => {
-        try {
-            console.log('Jugador desconectado', cliente.id);
-            let personaBorrada = jugadores.borrarJugador(cliente.id);
-            cliente.broadcast.to(personaBorrada.sala).emit('iniciarPartida', jugadores.getJugadoresPorSala(personaBorrada.sala));
+     });
+     cliente.on('jugada', (data, callback = Function) => {
+         console.log(data);
+         cliente.broadcast.to(data.sala).emit('jugada', data);
+         //callback(data);
+     });
+     cliente.on('ganador', (data, callback = Function) => {
+         console.log(data);
+         cliente.broadcast.to(data.sala).emit('ganador', data);
+         //callback(data);
+     });
+     cliente.on('reniciar', (data, callback = Function) => {
+         console.log(data);
+         cliente.broadcast.to(data.sala).emit('reiniciar', data);
+         //callback(data);
+     });
+     cliente.on('disconnect', () => {
+         try {
+             console.log('Jugador desconectado', cliente.id);
+             let personaBorrada = jugadores.borrarJugador(cliente.id);
+             cliente.broadcast.to(personaBorrada.sala).emit('iniciarPartida', jugadores.getJugadoresPorSala(personaBorrada.sala));
 
-        } catch (err) {
-
-
-        }
-    });
-
-
-
-
-}
+         } catch (err) {
 
 
+         }
+     });
 
-module.exports = {
-    socketController
-}
+
+
+
+ }
+
+
+
+ module.exports = {
+     socketController
+ }
